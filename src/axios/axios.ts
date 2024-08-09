e4191bd37ad6f153eaf4d45692ff6dd4fcd99aea
Prabin <prabinmoktan07@gmail.com>
2024-08-09 14:25:33 +0300

    build error fix

--- src/axios/axios.ts
+++ src/axios/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import  Cookies  from 'js-cookie';
+// import  Cookies  from 'js-cookie';
 
 // const abortController = new AbortController();
 export const axiosInstance = axios.create({
